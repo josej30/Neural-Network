b32d0f3fd4921840045f3df8d7e58250038a65c1
Jose <josej30@gmail.com>
2011-11-04 20:12:54 -0430

    adding the visualization scripts with gnuplot. Adding the FileWriter class. modifying the main so it can write the output classifications to the files

--- BackPropagation.java
+++ BackPropagation.java
@@ -16,28 +16,6 @@ public class BackPropagation {
      */
     public static void main(String[] args) {
 
-        /* XOR INPUT <---- Have to be erased */
-
-        double [][] x = new double[4][2];
-
-        x[0][0] = 0;
-        x[0][1] = 0;
-        x[1][0] = 0;
-        x[1][1] = 1;
-        x[2][0] = 1;
-        x[2][1] = 0;
-        x[3][0] = 1;
-        x[3][1] = 1;
-
-        double [] y = new double[4];
-
-        y[0] = 0;
-        y[1] = 1;
-        y[2] = 1;
-        y[3] = 0;
-
-        /* End XOR INPUT <---- Have to be erased */
-
         // Reading the input file
         FileReader fr = new FileReader("500.txt");
         fr.read();
@@ -45,24 +23,28 @@ public class BackPropagation {
         double [][] X = fr.returnX();
         double [] Y = fr.returnY();
 
+        boolean output_file = true;
         
+        int num_neu = 2;
+        int num_out = 1;
+        int size_in = 2;
+
         double eta = 0.1;
         int runs = 10000;
 
-        /*
-         * 2 = neurons on the hidden layer
-         * 2 = size a single input example
-         * 1 = neuron on the output layer
-         */
-        NeuralNetwork nn = new NeuralNetwork(4,2,1,X,Y,eta);
+        NeuralNetwork nn = new NeuralNetwork(num_neu,size_in,num_out,X,Y,eta);
 
         nn.initialize();    // Randomizing the weights
         nn.run(runs);       // Running the algorithm with runs iterations
 
-        System.out.println("Running the Neural Network "+runs+" iterations\n");
+        System.out.println("Running the Neural Network "+runs+
+                " iterations and "+num_neu+" neurons in the hidden layer\n");
 
         int right = 0;
         int wrong = 0;
+
+        FileWriter fw_right = new FileWriter("right.txt");
+        FileWriter fw_wrong = new FileWriter("wrong.txt");
 
         for (int i=0; i<X.length; i++) {
 
@@ -72,11 +54,34 @@ public class BackPropagation {
             // Neural Network output
             double ans = nn.net_out[0];
 
-            if (((ans > 0.5) && (Y[i]==1.0)) || ((ans < 0.5) && (Y[i]==0.0)))
+            if (((ans > 0.5) && (Y[i]==1.0)) || ((ans < 0.5) && (Y[i]==0.0))) {
                 right++;
-            else
+
+                if (output_file) {
+                for (int j=0; j<2 ; j++){
+                    fw_right.write(X[i][j]+" ");
+                }
+                fw_right.write("\n");
+                }
+
+            }
+            else {
                 wrong++;
 
+                if (output_file) {
+                for (int j=0; j<2 ; j++){
+                    fw_wrong.write(X[i][j]+" ");
+                }
+                fw_wrong.write("\n");
+                }
+
+            }
+
+        }
+
+        if (output_file) {
+                fw_right.closeFile();
+                fw_wrong.closeFile();
         }
 
         System.out.println("Right Classifications: "+right+ "("+(float)right/(float)X.length*100+"%)");
---
+++ gnuplot/plot.m
@@ -0,0 +1,6 @@
+set title "UBICACION DE PUNTOS Y ERROR"
+
+set origin 0,0
+
+plot "right.txt" lt 1, \
+"wrong.txt" lt 2
